668fb2307e2c3dfe50dc332f51a52b21721b58d0
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-06-22 23:27:53 -0500

    Done changes before refactor

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
-import {LitElement, html} from 'lit-element';
+import { LitElement, html, css } from "lit-element";
 
 class MyElement extends LitElement {
-  static get properties(){
+  static get properties() {
     return {
-      message: {type: String}
+      myBool: { type: Boolean },
     };
+  }
+  static get styles() {
+    return css`
+      div{
+        height:100px;
+        width:100px;
+        border:1px solid black;
+      }
+      .red {
+        background-color:red;
+      }
+      .blue {
+        background-color: blue;
+      }
+    `;
   }
   constructor() {
     super();
-    this.message = 'Loading';
-    this.addEventListener('stuff-loaded', (e) => { this.message = e.detail } );
-    this.loadStuff();
+    this.color='red';
+    this.myBool = true;
+    this.addEventListener("change-color", (e) => {
+      this.myBool = e.detail;
+    });
+    this.changeText();
   }
   render() {
-    return html`
-      <p>${this.message}</p>
+    return html` 
+      <div class=${this.color}> 
+      Im changing
+      </div>
+      <div class=${this.color}> 
+      Im changing
+      </div>
     `;
   }
-  loadStuff() {
+
+  changeText() {
     setInterval(() => {
-      let loaded = new CustomEvent('stuff-loaded', {
-        detail: 'Loading complete.'
+      let change = new CustomEvent("change-color", {
+        detail: !this.myBool,
       });
-      this.dispatchEvent(loaded);
-    }, 3000);
+      this.dispatchEvent(change);
+      this.myBool ? this.color='blue': this.color="red";
+    }, 1000);
   }
-  }
-  customElements.define('my-element', MyElement);  
+  
+}
+customElements.define("my-element", MyElement);
